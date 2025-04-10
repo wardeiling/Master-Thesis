8f61b07a2195a967bec2d3c0fbe25f938d6d55cd
Ward <104768852+wardeiling@users.noreply.github.com>
2025-04-10 13:46:01 +0200

    add proper error handling and clean up

--- scripts/hamaker2020_extended/helper-functions/model-fitting.R
+++ scripts/hamaker2020_extended/helper-functions/model-fitting.R
@@ -23,11 +23,32 @@ glmm_model_fitting <- function(data, outcome.type) {
     g4 = Y ~ X + X.cluster.means
   )
   
+  # Create NA output template (list with correct names)
+  coef_NA_fill <- list(
+    l1 = setNames(rep(NA, 2), c("(Intercept)", "X")),
+    l2 = setNames(rep(NA, 2), c("(Intercept)", "X.cent")),
+    l3a = setNames(rep(NA, 3), c("(Intercept)", "X.cent", "X.cluster.means")),
+    l4 = setNames(rep(NA, 3), c("(Intercept)", "X", "X.cluster.means"))
+  )
+  
   if (outcome.type == "continuous") {
     for (name in names(formulas)) {
       models[[name]] <- tryCatch(
+        
+        # Use MLE (faster and we are not interested in variance components)
         fixef(lmer(formulas[[name]], data = data, REML = FALSE)),
-        error = function(e) NA
+        
+        # Catch warnings and errors in lmer() and return NA (e.g., non-convergence, singularity)
+        warning = function(w) {
+          message("Warning in lmer: ", conditionMessage(w))
+          # Return NA for the coefficients
+          return(coef_NA_fill[[name]])
+        },
+        error = function(e) {
+          message("Error in lmer: ", conditionMessage(e))
+          # Return NA for the coefficients
+          return(coef_NA_fill[[name]])
+        }
       )
     }
   } else if (outcome.type == "binary") {
@@ -35,10 +56,21 @@ glmm_model_fitting <- function(data, outcome.type) {
     for (name in names(formulas)) {
       models[[name]] <- tryCatch(
         fixef(glmer(formulas[[name]], data = data, family = family_arg)),
-        error = function(e) NA
+        
+        # Catch warnings and errors in glmer() and return NA (e.g., non-convergence, singularity)
+        warning = function(w) {
+          message("Warning in glmer: ", conditionMessage(w))
+          # Return NA for the coefficients
+          return(coef_NA_fill[[name]])
+        },
+        error = function(e) {
+          message("Error in glmer: ", conditionMessage(e))
+          # Return NA for the coefficients
+          return(coef_NA_fill[[name]])
+        }
       )
     }
-    } 
+  } 
   
   # Fit GEE models with different correlation structures using geepack::geeglm
   cor_structures <- c("independence", "exchangeable", "ar1")
@@ -47,8 +79,14 @@ glmm_model_fitting <- function(data, outcome.type) {
     for (cor in cor_structures) {
       model_name <- paste0("g.", cor, i)
       models[[model_name]] <- tryCatch(
-        coef(geeglm(gee_formulas[[i]], id = Cluster, data = data, corstr = cor, family = family_arg)),
-        error = function(e) NA
+        coef(geeglm(gee_formulas[[i]], id = Cluster, data = data, corstr = cor, family = family_arg, control = geese.control(maxit = 50))),
+        
+        # Catch errors in geeglm() and return NA (function does not have warnings!)
+        error = function(e) {
+          message("Error in geeglm: ", conditionMessage(e))
+          # Return NA for the coefficients
+          return(coef_NA_fill[[i]])
+        }
       )
     }
   }
