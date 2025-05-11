a0eddb2834a2c60adfcb10063e95028ae4c76197
Ward <104768852+wardeiling@users.noreply.github.com>
2025-05-11 16:39:18 +0200

    clean up repository

--- scripts/diffr-test.R
+++
@@ -1,8 +0,0 @@
-library(diffr)
-
-# Load the two files to compare
-diffr::diffr(file2 = "qian2020/updated scripts/generative_model_updated_newmodels.R", file1 = "qian2020/original scripts/generative_model.R", 
-             minJumpSize = 100)  # wordWrap = 100)
-
-diffr::diffr(file2 = "qian2020/updated scripts/simulation_updated_newmodels.R", file1 = "qian2020/original scripts/simulation.R", 
-             minJumpSize = 100)  # wordWrap = 100)
